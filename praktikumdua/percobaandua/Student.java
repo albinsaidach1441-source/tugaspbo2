2747b288137af1ab138c3cf6948aec31f119d368
achmad fauzi <albinsaidach1441@gmail.com>
2025-09-03 21:46:51 +0700

    Create Student.java

---
+++ praktikumdua/percobaandua/Student.java
@@ -0,0 +1,15 @@
+package percobaandua;
+
+class Student {
+    int rno;
+    String stName;
+    
+    Address stAddr;
+    Student(int roll, String name, Address address){
+        this.rno = roll;
+        this.stName = name;
+        this.stAddr = address;
+                
+    }
+    
+}
