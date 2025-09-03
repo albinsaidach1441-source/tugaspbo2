f7967dcbbdd18f3e2fbb02ead5a120649be2f7d9
achmad fauzi <albinsaidach1441@gmail.com>
2025-09-03 21:56:45 +0700

    Create Baterai.java

---
+++ praktikumdua/percobaantiga/Baterai.java
@@ -0,0 +1,10 @@
+package percobaantiga;
+
+class Baterai extends KomponenMobil {
+    public Baterai(String namaBaterai){
+        super(namaBaterai);
+    }
+    public void cekBaterai(){
+        System.out.println(namaKomponen + " diperiksa. ");
+    }
+}
