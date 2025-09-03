3893f626208ac5a22a42339bb1e32209788576d0
achmad fauzi <albinsaidach1441@gmail.com>
2025-09-03 21:42:11 +0700

    Create speed.java

---
+++ praktikumdua/percobaansatu/speed.java
@@ -0,0 +1,12 @@
+package percobaansatu;
+
+class Speed{
+    private double max;
+    
+    public double getMax(){
+        return max;
+    }
+    public void setMax(double max){
+        this.max = max;
+    }
+}
