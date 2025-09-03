e5698b81477698d50ec7a02deb79cc446abe5c70
achmad fauzi <albinsaidach1441@gmail.com>
2025-09-03 21:23:58 +0700

    Delete Programmer.java

--- Programmer.java
+++
@@ -1,9 +0,0 @@
-class Programmer extends Employee{
-    int bonus= 10000;
-    
-    public static void main(String[]args){
-            Programmer p=new Programmer();
-    System.out.println("Programmer salary is: "+p.salary);
-    System.out.println("Programmer bonus is: "+p.bonus);
-    }
-}
