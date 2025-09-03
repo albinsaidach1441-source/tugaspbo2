d074956e5bafd45b3539539756396f16d08cd034
achmad fauzi <albinsaidach1441@gmail.com>
2025-09-03 21:40:15 +0700

    Update vehicle.java

--- praktikumdua/percobaansatu/vehicle.java
+++ praktikumdua/percobaansatu/vehicle.java
@@ -1,1 +1,12 @@
+package percobaansatu;
 
+class vehicle{
+    private String vin;
+    
+    public String getVin(){
+        return vin;
+    }
+    public void setVin(String vin){
+        this.vin = vin;
+    }
+}
